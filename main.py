af6d19f872cb633427ecc32edd3514c3c058e4cf
Mark Melnic <markhappy200@gmail.com>
2020-10-02 16:58:43 +0200

    Reformatted using py black

--- main.py
+++ main.py
@@ -20,17 +20,21 @@ MARKETPLACE_LINK = "https://www.facebook.com/marketplace/category/propertyrental
 def boot():
     # manage notifications
     chrome_options = webdriver.ChromeOptions()
-    prefs = {"profile.default_content_setting_values.notifications" : 2}
-    chrome_options.add_experimental_option("prefs",prefs)
+    prefs = {"profile.default_content_setting_values.notifications": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
 
     # driver itself
-    dv = webdriver.Chrome(chrome_options = chrome_options, executable_path = r"./chromedriver.exe")
+    dv = webdriver.Chrome(
+        chrome_options=chrome_options, executable_path=r"./chromedriver.exe"
+    )
     dv.maximize_window()
     return dv
+
 
 # kill the driver
 def killb(dv):
     dv.quit()
+
 
 # login protocol
 def login(dv, username, password):
@@ -69,30 +73,42 @@ def messagingProcedure(dv, messages, links_file, processed_links):
     while True:
         page_body = dv.find_element_by_xpath("/html/body")
         page_body.send_keys(Keys.PAGE_DOWN)
-        soup = BeautifulSoup(dv.page_source, 'html.parser')
+        soup = BeautifulSoup(dv.page_source, "html.parser")
         listings = soup.find_all("a")
         for listing in listings:
-            if (listing['role'] == "link") and ("/marketplace/item/" in listing['href']):
-                if not MAIN_LINK + listing['href'] in processed_links:
+            if (listing["role"] == "link") and (
+                "/marketplace/item/" in listing["href"]
+            ):
+                if not MAIN_LINK + listing["href"] in processed_links:
                     try:
-                        processed_links.append(MAIN_LINK + listing['href'])
-                        links_file.write(MAIN_LINK + listing['href'] + "\n")
-                        dv.execute_script('window.open(arguments[0]);', MAIN_LINK + listing['href'])
-                        new_window = [window for window in dv.window_handles if window != current_window][0]
+                        processed_links.append(MAIN_LINK + listing["href"])
+                        links_file.write(MAIN_LINK + listing["href"] + "\n")
+                        dv.execute_script(
+                            "window.open(arguments[0]);", MAIN_LINK + listing["href"]
+                        )
+                        new_window = [
+                            window
+                            for window in dv.window_handles
+                            if window != current_window
+                        ][0]
                         dv.switch_to.window(new_window)
                         dv.implicitly_wait(5)
-                        contact_button = dv.find_element_by_xpath("//*[@id=\"mount_0_0\"]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div/div[1]/div[1]/div[3]/div/div[1]")
+                        contact_button = dv.find_element_by_xpath(
+                            '//*[@id="mount_0_0"]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div/div[1]/div[1]/div[3]/div/div[1]'
+                        )
                         contact_button.click()
-                        time.sleep(random.randint(2,4))
+                        time.sleep(random.randint(2, 4))
                         if contact_button.text == "Message":
                             mouse.position = (950, 650)
                             mouse.click(Button.left, 2)
                             time.sleep(1)
                             keyboard.type(random.choice(messages))
                             time.sleep(1)
-                            dv.find_element_by_xpath("//*[@id=\"mount_0_0\"]/div/div[1]/div[1]/div[4]/div/div/div[1]/div/div[2]/div/div/div/div[4]/div[2]/div").click()
+                            dv.find_element_by_xpath(
+                                '//*[@id="mount_0_0"]/div/div[1]/div[1]/div[4]/div/div/div[1]/div/div[2]/div/div/div/div[4]/div[2]/div'
+                            ).click()
 
-                        time.sleep(random.randint(6,10))
+                        time.sleep(random.randint(6, 10))
                         dv.close()
                         dv.switch_to.window(current_window)
                     except AttributeError:
@@ -108,7 +124,7 @@ if __name__ == "__main__":
             messages = msg_file.read().splitlines()
         dv = boot()
 
-        with open("login_credentials.txt", "r", newline = '') as creds_file:
+        with open("login_credentials.txt", "r", newline="") as creds_file:
             creds = creds_file.read().splitlines()
         login(dv, creds[0], creds[1])
 
